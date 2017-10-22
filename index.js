b61a26c0097a858cf0635c047c1656816cef3e5f
April Rivera <aprilcr87@gmail.com>
2017-10-22 13:23:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a,b){
+  return `${a + b}`
+}
+add(3,4);
+function subtract(a,b){
+  return `${a - b}`
+}
+subtract(10, 5);
+function multiply(a,b){
+  return `${a * b}`
+}
+multiply(3, 4);
+function divide(a,b){
+  return `${a / b}`
+}
+divide(10, 2);
+function inc(n){
+  return n = n+1
+}
+inc(2);
+function dec(n){
+  return n = n-1
+}
+dec(2);
